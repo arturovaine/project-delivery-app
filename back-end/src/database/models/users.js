da642ee077e9cb26f79a9568095bf2a3747af029
Victor Araujo <victor_emanuel9@hotmail.com>
2022-07-13 19:41:56 -0300

    chore: user model

---
+++ back-end/src/database/models/users.js
@@ -0,0 +1,39 @@
+'use strict';
+import db from '.';
+// import { Sequelize, DataTypes } from 'sequelize';
+
+export const UserModel = (sequelize, DataTypes) => {
+
+  const User = sequelize.define('User', {
+    id: {
+      primaryKey: true,
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+    },
+    name: {
+      type: DataTypes.STRING(100),
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING(100),
+      allowNull: false,
+      unique: true,
+    },
+    password: {
+      type: DataTypes.STRING(32),
+      allowNull: false,
+    },
+    role: {
+      type: DataTypes.STRING(20),
+      allowNull: false,
+    },
+  }, {
+    sequelize: db,
+    modelName: 'users',
+    timestamps: false,
+  });
+
+  return User;
+}
+
+ 
