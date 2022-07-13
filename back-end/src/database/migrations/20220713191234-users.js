985a04bdeddd5ec1da77a22f94a46af313f1e517
Victor Araujo <victor_emanuel9@hotmail.com>
2022-07-13 16:32:37 -0300

    feat: migration user

---
+++ back-end/src/database/migrations/20220713191234-users.js
@@ -0,0 +1,35 @@
+'use strict';
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.createTable('users', { 
+      id: {
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      password: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      role: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+
+    });
+  },
+
+  async down (queryInterface, Sequelize) {
+    await queryInterface.dropTable('users'); 
+  }
+};
