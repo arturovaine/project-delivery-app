e6fbd36edb1b6972815e874f0b8511e6e9cf9974
Victor Araujo <victor_emanuel9@hotmail.com>
2022-07-13 19:47:13 -0300

    chore: product model

---
+++ back-end/src/database/models/products.js
@@ -0,0 +1,38 @@
+'use strict';
+import db from '.';
+// import { Sequelize, DataTypes } from 'sequelize';
+
+export const UserModel = (sequelize, DataTypes) => {
+
+  const Product = sequelize.define('Product', {
+    id: {
+      primaryKey: true,
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+    },
+    name: {
+      type: DataTypes.STRING(100),
+      unique: true,
+      allowNull: false,
+    },
+    price: {
+      type: DataTypes.DECIMAL(4,2),
+      allowNull: false,
+    },
+    urlImage: {
+      type: DataTypes.STRING(200),
+      allowNull: false,
+      defaultValue: '',
+      field: 'url_image',
+    }
+  }, {
+    sequelize: db,
+    modelName: 'products',
+    timestamps: false,
+    underscored: true,
+  });
+
+  return Product;
+}
+
+ 
