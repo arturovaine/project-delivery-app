ee0f9045e951c5136c5d569efdde0cf3614b1bf2
Felipe Matheus <fmatheusog@gmail.com>
2022-07-14 17:07:11 -0300

    refact: change register page to follow Button and TextInput components changes

--- front-end/src/pages/Register/index.js
+++ front-end/src/pages/Register/index.js
@@ -7,38 +7,42 @@ const RegisterPage = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
-
   return (
-    <>
-      <div id="register-form">
-        <TextInput
-          label="Nome"
-          testId="common_register__input-name"
-          password={ false }
-          placeholder="Seu nome"
-        />
-        <TextInput
-          label="E-mail"
-          testId="common_register__input-email"
-          password={ false }
-          placeholder="seu-email@site.com.br"
-        />
-        <TextInput
-          label="Senha"
-          testId="common_register__input-password"
-          password={ true }
-          placeholder="Sua senha"
-        />
+    <form id="register-form">
+      <TextInput
+        label="Nome"
+        testId="common_register__input-name"
+        onChange={ ({ target: { value } }) => setName(value) }
+        password={ false }
+        placeholder="Seu nome"
+        value={ name }
+      />
+      <TextInput
+        label="E-mail"
+        testId="common_register__input-email"
+        onChange={ ({ target: { value } }) => setEmail(value) }
+        password={ false }
+        placeholder="seu-email@site.com.br"
+        value={ email }
+      />
+      <TextInput
+        label="Senha"
+        testId="common_register__input-password"
+        onChange={ ({ target: { value } }) => setPassword(value) }
+        password
+        placeholder="Sua senha"
+        value={ password }
+      />
 
-        <Button
-          label="Login"
-          style=""
-          testId="common_register__button-register"
-          onClick={ null }
-        />
-      </div>
-    </>
-  )
-}
+      <Button
+        disabled={ false }
+        label="CADASTRAR"
+        buttonType="primary-button"
+        testId="common_register__button-register"
+        onClick={ () => console.log('Not implemented') }
+      />
+    </form>
+  );
+};
 
 export default RegisterPage;
