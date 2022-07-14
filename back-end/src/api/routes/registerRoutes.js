07ce7c30120141ed6a8c5e3d6a528410b07de921
Victor Araujo <victor_emanuel9@hotmail.com>
2022-07-14 19:33:08 -0300

    fix(routes): /register

--- back-end/src/api/routes/registerRoutes.js
+++ back-end/src/api/routes/registerRoutes.js
@@ -1,8 +1,8 @@
 const express = require('express');
-
-const router = express.Router();
 const ControllerUsers = require('../controllers/ControllerUsers');
 
-router.post('/register', ControllerUsers.createUser);
+const router = express.Router();
+
+router.post('/', ControllerUsers.createUser);
 
 module.exports = router;
