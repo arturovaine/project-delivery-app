10c55decce12ea01a81646c662bda87944978ece
Arturo Vaine <arturo.vaine@gmail.com>
2022-07-16 18:07:59 -0300

    fix: reescrita dos títulos dos testes de data-testid e Login e Register

--- front-end/src/tests/login.test.js
+++ front-end/src/tests/login.test.js
@@ -4,19 +4,19 @@ import { render, screen } from '@testing-library/react';
 import Login from '../pages/Login';
 
 describe('Verifica se os elementos da tela de login estão com os data-testids corretos', () => {
-  test('Verifica o input de e-mail, na tela de login, com o data-testid correto.', () => {
+  test('Verifica se existe data-testid para elemento de input de e-mail de login (email).', () => {
     render(<Login />);
     const inputEmail = screen.getByTestId('common_login__input-email');
     expect(inputEmail).toBeInTheDocument();
   });
 
-  test('Verifica o input de password, na tela de login, com o data-testid correto.', () => {
+  test('Verifica se existe data-testid para elemento de input de senha do login (password).', () => {
     render(<Login />);
     const inputPassword = screen.getByTestId('common_login__input-password');
     expect(inputPassword).toBeInTheDocument();
   });
 
-  test('Verifica o botão de login, na tela de login, com o data-testid correto.', () => {
+  test('Verifica se existe data-testid para elemento de botão de login.', () => {
     render(<Login />);
     const btnLogin = screen.getByTestId('common_login__button-login');
     expect(btnLogin).toBeInTheDocument();
--- front-end/src/tests/register.test.js
+++ front-end/src/tests/register.test.js
@@ -8,13 +8,26 @@ const MOCK_EMAIL = 'mail@example.com';
 const MOCK_PASSWORD = 'password_example';
 
 describe('Verifica se os elementos da tela de cadastro estão com os data-testids corretos', () => {
-  test('Verifica data-testid do botão de cadastro.', () => {
+  test('Verifica se existe data-testid para elemento de cadastro de nome (name).', () => {
     render(<Register />);
-    const btnRegister = screen.getByTestId('common_login__button-register');
+    const inputName = screen.getByTestId('common_register__input-name');
+    expect(inputName).toBeInTheDocument();
+  });
+
+  test('Verifica se existe data-testid para elemento de cadastro de e-mail (email).', () => {
+    render(<Register />);
+    const inputEmail = screen.getByTestId('common_register__input-email');
+    expect(inputEmail).toBeInTheDocument();
+  });
+  
+  test('Verifica se existe data-testid para elemento de botão de cadastro de nome.', () => {
+    render(<Register />);
+    // const btnRegister = screen.getByTestId('common_login__button-register'); verificar se data-testid está correto
+    const btnRegister = screen.getByTestId('common_register__button-register');
     expect(btnRegister).toBeInTheDocument();
   });
 
-  test('Verifica data-testid do elemento de erro de cadastro.', () => {
+  test('Verifica se existe data-testid do elemento de erro de cadastro.', () => {
     render(<Register />);
     
     const inputName = screen.getByTestId('common_register__input-name');
@@ -27,7 +40,7 @@ describe('Verifica se os elementos da tela de cadastro estão com os data-testid
     userEvent.type(inputEmail, MOCK_EMAIL);
     userEvent.type(inputPassword, MOCK_PASSWORD);
     
-    const btnRegister = screen.getByTestId('common_login__button-register');
+    const btnRegister = screen.getByTestId('common_register__button-register');
     userEvent.click(btnRegister);
     expect(elementInvalidRegister).toBeInTheDocument();
 
