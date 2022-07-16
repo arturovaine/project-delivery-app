ede511e0d115a6b0616e563e169732ea3246ea66
Arturo Vaine <arturo.vaine@gmail.com>
2022-07-16 13:39:11 -0300

    fix: seletor getAllByTestId

--- front-end/src/tests/seller.test.js
+++ front-end/src/tests/seller.test.js
@@ -14,97 +14,97 @@ describe('Verifica se os elementos da tela de vendedor estão com os data-testid
 
   test('Verifica data-testid ...', () => {
     // render(<Seller />);
-    const elOrderId = screen.getByTestId('seller_orders__element-order-id-');
+    const elOrderId = screen.getAllByTestId('seller_orders__element-order-id-');
     expect(elOrderId).toBeInTheDocument();
   });
 
   test('Verifica data-testid ...', () => {
     // render(<Seller />);
-    const elDelivStatus = screen.getByTestId('seller_orders__element-delivery-status-');
+    const elDelivStatus = screen.getAllByTestId('seller_orders__element-delivery-status-');
     expect(elDelivStatus).toBeInTheDocument();
   });
 
   test('Verifica data-testid ...', () => {
     // render(<Seller />);
-    const elOrderDate = screen.getByTestId('seller_orders__element-order-date-');
+    const elOrderDate = screen.getAllByTestId('seller_orders__element-order-date-');
     expect(elOrderDate).toBeInTheDocument();
   });
 
   test('Verifica data-testid ...', () => {
     // render(<Seller />);
-    const elCardPrice = screen.getByTestId('seller_orders__element-card-price-');
+    const elCardPrice = screen.getAllByTestId('seller_orders__element-card-price-');
     expect(elCardPrice).toBeInTheDocument();
   });
 
   test('Verifica data-testid ...', () => {
     // render(<Seller />);
-    const elCardAddress = screen.getByTestId('seller_orders__element-card-address-');
+    const elCardAddress = screen.getAllByTestId('seller_orders__element-card-address-');
     expect(elCardAddress).toBeInTheDocument();
   });
 
   test('Verifica data-testid ...', () => {
     // render(<Seller />);
-    const elLabelOrderId = screen.getByTestId('seller_order_details__element-order-details-label-order-id');
+    const elLabelOrderId = screen.getAllByTestId('seller_order_details__element-order-details-label-order-id');
     expect(elLabelOrderId).toBeInTheDocument();
   });
 
   test('Verifica data-testid ...', () => {
     // render(<Seller />);
-    const elLabelDelivStat = screen.getByTestId('seller_order_details__element-order-details-label-delivery-status');
+    const elLabelDelivStat = screen.getAllByTestId('seller_order_details__element-order-details-label-delivery-status');
     expect(elLabelDelivStat).toBeInTheDocument();
   });
 
   test('Verifica data-testid ...', () => {
     // render(<Seller />);
-    const elLabelOrderDate = screen.getByTestId('seller_order_details__element-order-details-label-order-date');
+    const elLabelOrderDate = screen.getAllByTestId('seller_order_details__element-order-details-label-order-date');
     expect(elLabelOrderDate).toBeInTheDocument();
   });
 
   test('Verifica data-testid ...', () => {
     // render(<Seller />);
-    const btnPrepCheck = screen.getByTestId('seller_order_details__button-preparing-check');
+    const btnPrepCheck = screen.getAllByTestId('seller_order_details__button-preparing-check');
     expect(btnPrepCheck).toBeInTheDocument();
   });
 
   test('Verifica data-testid ...', () => {
     // render(<Seller />);
-    const btnDispatCheck = screen.getByTestId('seller_order_details__button-dispatch-check');
+    const btnDispatCheck = screen.getAllByTestId('seller_order_details__button-dispatch-check');
     expect(btnDispatCheck).toBeInTheDocument();
   });
 
   test('Verifica data-testid ...', () => {
     // render(<Seller />);
-    const elTabItemNum = screen.getByTestId('seller_order_details__element-order-table-item-number-');
+    const elTabItemNum = screen.getAllByTestId('seller_order_details__element-order-table-item-number-');
     expect(elTabItemNum).toBeInTheDocument();
   });
 
   test('Verifica data-testid ...', () => {
     // render(<Seller />);
-    const elTabName = screen.getByTestId('seller_order_details__element-order-table-name-');
+    const elTabName = screen.getAllByTestId('seller_order_details__element-order-table-name-');
     expect(elTabName).toBeInTheDocument();
   });
 
   test('Verifica data-testid ...', () => {
     // render(<Seller />);
-    const elTabQty = screen.getByTestId('seller_order_details__element-order-table-quantity-');
+    const elTabQty = screen.getAllByTestId('seller_order_details__element-order-table-quantity-');
     expect(elTabQty).toBeInTheDocument();
   });
 
   test('Verifica data-testid ...', () => {
     // render(<Seller />);
-    const elTabUnitPrice = screen.getByTestId('seller_order_details__element-order-table-unit-price-');
+    const elTabUnitPrice = screen.getAllByTestId('seller_order_details__element-order-table-unit-price-');
     expect(elTabUnitPrice).toBeInTheDocument();
   });
 
   test('Verifica data-testid ...', () => {
     // render(<Seller />);
-    const elTabSubtotal = screen.getByTestId('seller_order_details__element-order-table-sub-total-');
+    const elTabSubtotal = screen.getAllByTestId('seller_order_details__element-order-table-sub-total-');
     expect(elTabSubtotal).toBeInTheDocument();
   });
 
   test('Verifica data-testid ...', () => {
     // render(<Seller />);
-    const elTotalPrice = screen.getByTestId('seller_order_details__element-order-total-price');
+    const elTotalPrice = screen.getAllByTestId('seller_order_details__element-order-total-price');
     expect(elTotalPrice).toBeInTheDocument();
   });
 });
