159cba18e2477c828bebb13691d31709e522e3e5
Victor Araujo <victor_emanuel9@hotmail.com>
2022-07-16 14:33:16 -0300

    chore: setup product model

---
+++ back-end/src/api/models/productsModel.js
@@ -0,0 +1,19 @@
+const { Product } = require('../../database/models');
+
+const create = () => {};
+
+const readAll = () => {};
+
+const readOne = () => {};
+
+const update = () => {};
+
+const deleteOne = () => {};
+
+module.exports = {
+  create,
+  readAll,
+  readOne,
+  update,
+  deleteOne,
+};
