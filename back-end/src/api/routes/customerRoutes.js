7f28aeba29d9a7b1719034f222283b3fcb82539a
Victor Araujo <victor_emanuel9@hotmail.com>
2022-07-20 16:06:35 -0300

    style: added comment

--- back-end/src/api/routes/customerRoutes.js
+++ back-end/src/api/routes/customerRoutes.js
@@ -4,6 +4,7 @@ const router = express.Router();
 
 const ControllerSales = require('../controllers/ControllerSales');
 
+// router.get('/orders');
 router.get('/orders/:id', ControllerSales.findSaleAndProducts);
 
 module.exports = router;
