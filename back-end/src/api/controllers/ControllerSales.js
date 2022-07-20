3554b37566f6436645f9c0a17d9fa35b9a29b16d
Victor Araujo <victor_emanuel9@hotmail.com>
2022-07-19 23:47:16 -0300

    feat: findSaleAndProducts implemented

--- back-end/src/api/controllers/ControllerSales.js
+++ back-end/src/api/controllers/ControllerSales.js
@@ -13,6 +13,20 @@ const createSaleWithProducts = async (req, res) => {
   }
 };
 
+const findSaleAndProducts = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const data = await salesService.findSaleAndProducts(id);
+    return res.status(200).json(data);
+   } catch (error) {
+     if (error instanceof CustomErrors) {
+       return res.status(error.statusCode).send({ error: error.message });
+     }
+     return res.status(500).send({ error: error.message });
+   }
+};
+
 module.exports = {
   createSaleWithProducts,
+  findSaleAndProducts,
 };
