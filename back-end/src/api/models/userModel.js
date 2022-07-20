ee8a77a87e4a661aee750477dff3c4be89fd5235
Victor Araujo <victor_emanuel9@hotmail.com>
2022-07-20 15:26:24 -0300

    revert(userModel): had to revert modification

--- back-end/src/api/models/userModel.js
+++ back-end/src/api/models/userModel.js
@@ -12,7 +12,6 @@ const createUser = async (name, email, password, role = 'customer') => {
 const findUser = async (email) => {
   try {
     const userExists = await User.findOne({ where: { email } });
-    if (!userExists) throw new Error('User not found');
     return userExists;
   } catch (err) {
     throw new Error(err.message);
