f3596da2f37e42fb7a643f2b1821c797c61c7b31
Victor Araujo <victor_emanuel9@hotmail.com>
2022-07-20 14:41:10 -0300

    feat(salesRoute): added middleware

--- back-end/src/api/routes/salesRoutes.js
+++ back-end/src/api/routes/salesRoutes.js
@@ -3,7 +3,8 @@ const express = require('express');
 const router = express.Router();
 
 const ControllerSales = require('../controllers/ControllerSales');
+const { verifySalesCheckoutData } = require('../middlewares');
 
-router.post('/checkout', ControllerSales.createSaleWithProducts);
+router.post('/checkout', verifySalesCheckoutData, ControllerSales.createSaleWithProducts);
 
 module.exports = router;
