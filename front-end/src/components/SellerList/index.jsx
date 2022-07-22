be2114e8aa29e32a62568419d52c38673be54942
Victor Araujo <victor_emanuel9@hotmail.com>
2022-07-22 18:45:13 -0300

    feat: component seller list

---
+++ front-end/src/components/SellerList/index.jsx
@@ -0,0 +1,36 @@
+import React, { useEffect, useState } from 'react';
+import { getRequest } from '../../services/api';
+
+const SellerList = () => {
+  const [sellers, setSellers] = useState([]);
+
+  useEffect(() => {
+    const loadSellers = async () => {
+      const { token } = JSON.parse(localStorage.getItem('user'));
+      const sellerData = await getRequest('/sellers/list', token);
+      setSellers(sellerData);
+    };
+    loadSellers();
+  }, []);
+
+  return (
+    <label htmlFor="customer_checkout__select-seller">
+      P.Vendedora Responsável
+      <select
+        testId="customer_checkout__select-seller"
+      >
+        <option selected disabled value="null">selecione...</option>
+        { sellers.map((e) => (
+          <option
+            key={ e.id }
+            id={ e.id }
+            value={ e.name }
+          >
+            {e.name}
+          </option>)) }
+      </select>
+    </label>
+  );
+};
+
+export default SellerList;
