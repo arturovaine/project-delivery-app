719a290fa5ae7822df7cd79d6d5cc4b1fbeacb60
Victor Araujo <victor_emanuel9@hotmail.com>
2022-07-22 02:50:00 -0300

    feat: cartButton component

---
+++ front-end/src/components/ProductCartButton/index.jsx
@@ -0,0 +1,42 @@
+import React, { useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
+import PropTypes from 'prop-types';
+
+const ProductCartButton = (props) => {
+  const { totalPrice, setTotalPrice, products } = props;
+  const history = useHistory();
+
+  useEffect(() => {
+    const calculateTotalPrice = () => {
+      let total = 0;
+      products.forEach((e) => {
+        const { quantity, price } = e;
+        total += quantity * parseFloat(price);
+      });
+      setTotalPrice(total.toFixed(2));
+    };
+    calculateTotalPrice();
+  }, [products]);
+
+  return (
+    <button
+      type="button"
+      data-testid="customer_products__checkout-bottom-value"
+      onClick={ () => history.push('/customer/checkout') }
+    >
+      Ver carrinho: R$
+      <span>{ totalPrice }</span>
+    </button>
+  );
+};
+
+ProductCartButton.propTypes = {
+  totalPrice: PropTypes.number.isRequired,
+  setTotalPrice: PropTypes.func.isRequired,
+  products: PropTypes.arrayOf(PropTypes.shape({
+    id: PropTypes.number,
+    quantity: PropTypes.number,
+  })).isRequired,
+};
+
+export default ProductCartButton;
