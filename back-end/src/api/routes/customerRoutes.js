d29058bfd103d8965b2ec9e1eed1664d63ac8cb3
Victor Araujo <victor_emanuel9@hotmail.com>
2022-07-24 20:34:39 -0300

    change: added verify email only to customer/orders/

--- back-end/src/api/routes/customerRoutes.js
+++ back-end/src/api/routes/customerRoutes.js
@@ -3,9 +3,9 @@ const express = require('express');
 const router = express.Router();
 
 const ControllerSales = require('../controllers/ControllerSales');
-const { verifyToken } = require('../middlewares');
+const { verifyToken, verifyOnlyEmail } = require('../middlewares');
 
-router.get('/orders', verifyToken, ControllerSales.findAllCustomerSales);
+router.get('/orders', verifyToken, verifyOnlyEmail, ControllerSales.findAllCustomerSales);
 router.get('/orders/:id', verifyToken, ControllerSales.findSaleAndProducts);
 
 module.exports = router;
