7de7cdc013275a37295521a313cd3f0c2e561820
Victor Araujo <victor_emanuel9@hotmail.com>
2022-07-25 15:56:53 -0300

    feat: added route GET /sellers/order

--- back-end/src/api/routes/sellerRoutes.js
+++ back-end/src/api/routes/sellerRoutes.js
@@ -6,5 +6,7 @@ const ControllerSeller = require('../controllers/ControllerSeller');
 const { verifyToken } = require('../middlewares');
 
 router.get('/list', verifyToken, ControllerSeller.findAllSellers);
+router.get('/orders', verifyToken, ControllerSeller.findAllOrders);
+// router.get('/orders/:id');
 
 module.exports = router;
