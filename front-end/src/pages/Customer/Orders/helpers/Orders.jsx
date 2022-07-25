f5ed09f78a2c078380d60185486133b3bd3c7c93
Victor Araujo <victor_emanuel9@hotmail.com>
2022-07-24 21:31:50 -0300

    feat: added helper component Orders

---
+++ front-end/src/pages/Customer/Orders/helpers/Orders.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Orders = ({ orderNumber, status, date, totalPrice }) => {
+  const totalPriceWithComma = () => totalPrice.replace('.', ',');
+  const transformDate = () => {
+    const nDate = date.split('T')[0];
+
+    return nDate.split('-').reverse().join('/');
+  };
+
+  return (
+    <section>
+      <div
+        data-testid={ `customer_orders__element-order-id-${orderNumber}` }
+      >
+        {orderNumber}
+      </div>
+      <div
+        data-testid={ `customer_orders__element-delivery-status-${orderNumber}` }
+      >
+        {status}
+      </div>
+      <div>
+        <div
+          data-testid={ `customer_orders__element-order-date-${orderNumber}` }
+        >
+          {transformDate()}
+        </div>
+        <div
+          data-testid={ `customer_orders__element-card-price-${orderNumber}` }
+        >
+          {totalPriceWithComma()}
+        </div>
+      </div>
+    </section>
+  );
+};
+
+Orders.propTypes = {
+  orderNumber: PropTypes.number.isRequired,
+  status: PropTypes.string.isRequired,
+  date: PropTypes.string.isRequired,
+  totalPrice: PropTypes.string.isRequired,
+};
+
+export default Orders;
