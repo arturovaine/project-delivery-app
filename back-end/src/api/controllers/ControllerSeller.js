9ab6f4189108dbe94c32f5f00f72de15af6db498
Victor Araujo <victor_emanuel9@hotmail.com>
2022-07-25 15:57:17 -0300

    feat: findAllOrders implemented

--- back-end/src/api/controllers/ControllerSeller.js
+++ back-end/src/api/controllers/ControllerSeller.js
@@ -1,5 +1,5 @@
 const CustomErrors = require('../errors/customErrors');
-const { findAllSellersService } = require('../services/ServiceSeller');
+const { findAllSellersService, findAllRelatedOrders } = require('../services/ServiceSeller');
 
 const findAllSellers = async (_req, res) => {
   try {
@@ -13,4 +13,17 @@ const findAllSellers = async (_req, res) => {
   }
 };
 
-module.exports = { findAllSellers };
+const findAllOrders = async (req, res) => {
+  try {
+    const { email } = res.locals;
+    const sales = await findAllRelatedOrders(email);
+    return res.status(200).json(sales);
+  } catch (error) {
+    if (error instanceof CustomErrors) {
+      return res.status(error.statusCode).send({ error: error.message });
+    }
+    return res.status(500).end();
+  }
+};
+
+module.exports = { findAllSellers, findAllOrders };
